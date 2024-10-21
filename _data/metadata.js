dc1b51ff548a77f4252e8edc4cbd2b9b886a5c09
Alice Kile <buoyantair@protonmail.com>
2024-10-22 00:36:14 +0200

    update

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Alice Kile",
+  url: "https://alicekile.dev/",
+  language: "en",
+  description: "Mostly writing about my personal coding projects",
+  author: {
+    name: "Alice Kile",
+    email: "buoyantair@protonmail.com",
+    url: "https://alicekile.dev/about/"
+  }
+}
